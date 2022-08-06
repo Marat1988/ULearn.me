69900ea0a5bbc3f90375d667b13576fb7256065c
Marat <pirat03071988@mail.ru>
2022-08-06 18:04:59 +0200

    Initial commit

---
+++ Часть 1. Основы программирования на примере C#/2.Variable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerNumber = 45; //Объявление переменной тиипа int
+            double doubleNumber=34.56; //Объявление переменной типа double
+            integerNumber = (int)doubleNumber; //Явноя конверсия типов
+            integerNumber = (int)Math.Round(34.67);
+            long longInteger = 4000000000;
+            integerNumber = (int)longInteger;
+            //При такой конверсии происходит ошибка переполнения, которая, однако,
+            //остается незамеченной для компилятора и среды разработки
+
+            //Таким образом можно отловить эти ошибки явно
+             checked
+             {
+                 integerNumber = (int)longInteger;
+             }
+        }
+    }
+}
