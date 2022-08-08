160ea92ecd33bd75656a14eb5791d74bebc57728
Marat <pirat03071988@mail.ru>
2022-08-08 20:35:47 +0200

    Update

---
+++ Часть 1. Основы программирования на примере C#/3.Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "Hello, world!";
+            string s = "Hello" + " " + "world";
+            char c = myString[1];
+            char myChar = 'e';
+            Console.WriteLine(myString.Length);
+            myString = myString.Substring(0, 5);
+            Console.WriteLine(myString);
+            string strangeSymbols = "© 2014 Σγμβόλσ";
+            myString = null;
+            int number = int.Parse("42");
+            string numString = number.ToString();//Из числа в строку
+            double number2 = double.Parse("34.42",CultureInfo.InvariantCulture);
+            //Следующий вызов не зависит от настроек и всегода использует точку в качестве размедителя
+            string invariantNumber2 = number2.ToString(CultureInfo.InvariantCulture);
+            Console.WriteLine(invariantNumber2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Часть 1. Основы программирования на примере C#/4.Var/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 45;
+            double angle = 1.4;
+            int c = (a + b) / 2;
+            Console.WriteLine(Math.Sin(angle));
+            var d = a - b;
+            /*Часто понятно, какого типа должна быть переменная. В этом случае можно писать var.
+             Компилятор самостоятельно догадатеся, что именно вы имели ввиду*/
+            var e = a / 2;
+            var f = a / 2.0;
+            c = a = b;
+            a -= 4;
+            a++;
+            a--;
+            ++a;
+            a = 5;
+            Console.WriteLine(a++);
+            //Выведет 5
+            a = 5;
+            Console.WriteLine(++a);
+            //выведет 6
+            Console.ReadKey();
+
+        }
+    }
+}
