d8d0890b6d564dc2a5948ae013bb29c229716b66
Marat <pirat03071988@mail.ru>
2022-08-11 00:15:16 +0200

    Insert

---
+++ Часть 1. Основы программирования на примере C#/11.Billiard/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Billiard
+{
+    class Program
+    {
+        /*Реализуйте метод для расчета угла отскока шарика от стены. Считайте, что угол падения равен углу отражения, 
+         * то есть можно пренебречь всеми физическими эффектами, связанными с кручением шаров, трением шара об стенку и т.п.*/
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return 2 * wallInclinationRadians - directionRadians;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
