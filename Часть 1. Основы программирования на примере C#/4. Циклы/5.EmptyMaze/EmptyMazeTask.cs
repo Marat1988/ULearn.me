63dca6b52713b802881976a2d0abe8da7eb84164
Марат Тухтаров <108996479+Marat1988@users.noreply.github.com>
2022-09-01 23:21:21 +0200

    Update EmptyMazeTask.cs

--- Часть 1. Основы программирования на примере C#/4. Циклы/5.EmptyMaze/EmptyMazeTask.cs
+++ Часть 1. Основы программирования на примере C#/4. Циклы/5.EmptyMaze/EmptyMazeTask.cs
@@ -1,10 +1,10 @@
-﻿namespace Mazes
+namespace Mazes
 {
 	public static class EmptyMazeTask
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
-            for (int i = robot.X; i < width - 2; i++)
+			for (int i = robot.X; i < width - 2; i++)
 				robot.MoveTo(Direction.Right);
 			for (int i = robot.Y; i < height - 2; i++)
 				robot.MoveTo(Direction.Down);
