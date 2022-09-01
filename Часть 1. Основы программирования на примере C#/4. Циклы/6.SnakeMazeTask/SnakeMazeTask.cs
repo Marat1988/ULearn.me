863080c872a30be3e822bd54da4c494e09cae198
Marat <pirat03071988@mail.ru>
2022-09-01 23:19:23 +0200

    INSERT

---
+++ Часть 1. Основы программирования на примере C#/4. Циклы/6.SnakeMazeTask/SnakeMazeTask.cs
@@ -0,0 +1,24 @@
+﻿namespace Mazes
+{
+	public static class SnakeMazeTask
+	{
+		private static void MoveDown(Robot robot)
+        {
+			for (int i = 0; i < 2; i++)
+				robot.MoveTo(Direction.Down);
+		}
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+            while (true)
+            {			
+				for (int i = robot.X; i < width - 2; i++)
+					robot.MoveTo(Direction.Right);
+				MoveDown(robot);
+				for (int i = robot.X; i > 1; i--)
+					robot.MoveTo(Direction.Left);
+				if (robot.Finished) break;
+				MoveDown(robot);
+			}
+		}
+	}
+}
