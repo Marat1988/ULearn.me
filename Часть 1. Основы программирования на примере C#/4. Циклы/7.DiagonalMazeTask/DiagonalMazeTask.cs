e9afb356070d19c0611a48e01c2594aa2bda07a8
Marat <pirat03071988@mail.ru>
2022-09-02 13:18:55 +0200

    INSERT

---
+++ Часть 1. Основы программирования на примере C#/4. Циклы/7.DiagonalMazeTask/DiagonalMazeTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			int numberMoves = (int)Math.Round((double)Math.Max(width, height) / Math.Min(width, height));
+            while (true)
+            {
+				for (int i = 0; i < numberMoves; i++)
+					robot.MoveTo(width > height ? Direction.Right : Direction.Down);
+				if (robot.Finished) 
+					break; 
+				else 
+					robot.MoveTo(width > height ? Direction.Down : Direction.Right);              
+			}
+		}
+	}
+}
