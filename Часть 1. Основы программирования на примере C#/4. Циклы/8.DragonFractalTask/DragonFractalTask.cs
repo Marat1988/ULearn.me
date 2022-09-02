a08f2953d2b3f0458e91931e0d52ff9c43d4ca4c
Marat <pirat03071988@mail.ru>
2022-09-02 14:08:23 +0200

    INSERT

---
+++ Часть 1. Основы программирования на примере C#/4. Циклы/8.DragonFractalTask/DragonFractalTask.cs
@@ -0,0 +1,25 @@
+﻿// В этом пространстве имен содержатся средства для работы с изображениями. 
+// Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+using System.Drawing;
+using System;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{
+		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+			double x = 1, y = 0;
+			for (int i = 0; i < iterationsCount; i++)
+			{
+				double x1;
+				double y1;
+				x1 = (x * Math.Cos(Math.PI / 4) - y * Math.Sin(Math.PI / 4)) / Math.Sqrt(2);
+				y1 = (x * Math.Sin(Math.PI / 4) + y * Math.Cos(Math.PI / 4)) / Math.Sqrt(2);
+				x = x1;
+				y = y1;
+				pixels.SetPixel(x, y);
+			}
+		}
+	}
+}
