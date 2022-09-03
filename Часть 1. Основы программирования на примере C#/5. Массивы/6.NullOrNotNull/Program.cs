d2f41119d1e45ee0c5c7a39b33d8c357c954bc7c
Marat <pirat03071988@mail.ru>
2022-09-04 01:56:04 +0200

    INSERT

---
+++ Часть 1. Основы программирования на примере C#/5. Массивы/6.NullOrNotNull/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.NullOrNotNull
+{
+    class Program
+    {
+
+        /*Вася написал метод, проверяющий, что первый элемент переданного массива равен 0. 
+         * Вася даже проверил, что ему не передали в качестве массива null. Но что-то все равно не работает.
+         * Исправьте ошибку и разберитесь, почему код не работал.*/
+
+        public static bool CheckFirstElement(int[] array)
+        {
+            return array != null && array.Length != 0 && array[0] == 0;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Часть 1. Основы программирования на примере C#/5. Массивы/7.BuildingInDegree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.BuildingInDegree
+{
+    class Program
+    {
+        /*Помогите Васе написать метод, который принимает массив int[], возводит все его элементы в
+         * заданную степень и возвращает массив с результатом этой операции.
+         * Исходный массив при этом должен остаться неизменным.*/
+        public static int[] GetPoweredArray(int[] arr, int power)
+        {
+            return arr.Select(x => (int)Math.Pow(x, power)).ToArray();
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
